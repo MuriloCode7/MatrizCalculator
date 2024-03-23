edc5b4cf0b0487d3f70743f6fa7ad6108d92b6aa
MuriloCode7 <murilomagalhaes60@gmail.com>
2024-03-22 21:07:49 -0300

    sync repositories

---
+++ library.c
@@ -0,0 +1,20 @@
+#include "library.h"
+
+#include <stdio.h>
+
+char matrizInput[100];
+
+void startMessage() {
+    printf("                     Exemplo de entrada:"
+           "\n                         1 2 3 -1"
+           "\n                         -1 1 0 2"
+           "\n                         2 -1 3 2"
+           "\n********************************************************************\n"
+           "Digite as entradas da matriz separando por espacos, linha por linha:\n");
+}
+
+void main() {
+
+    startMessage();
+
+}
